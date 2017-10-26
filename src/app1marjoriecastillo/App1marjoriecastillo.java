cc7e5f9a2af9f9ae007bcbba14930e14e09bec82
inescastillo1968 <inesmayo12345@hotmail.com>
2017-10-25 21:08:44 -0300

    esta listoooooo

---
+++ src/app1marjoriecastillo/App1marjoriecastillo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app1marjoriecastillo;
+
+/**
+ *
+ * @author LAB10
+ */
+public class App1marjoriecastillo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+      int numero1;
+      int numero2;
+      numero1=50;
+      numero2=90;
+      
+      System.out.println("suma de numero1 y numero2"+(numero1+numero2));
+      System.out.println("restar numero1 y numero2"+(numero1-numero2));
+      System.out.println("multiplicacion de numero1 y numero2"+(numero1*numero2));
+      System.out.println("division de numero1 y numero2"+(numero1/numero2));
+      System.out.println("resto del numero1 y numero2"+(numero1%numero2));
+              // TODO code application logic here
+    }
+    
+}
